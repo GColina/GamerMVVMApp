360b5891e775a0d002f1c30baaf3b3959f8186b2
Gabriel Colina <121101574+GColina@users.noreply.github.com>
2024-01-21 16:53:46 +0100

    .

--- app/src/main/java/com/kotlin_crew/gamermvvmapp/presentation/screens/signUp/SignUpScreen.kt
+++ app/src/main/java/com/kotlin_crew/gamermvvmapp/presentation/screens/signUp/SignUpScreen.kt
@@ -1,19 +1,46 @@
 package com.kotlin_crew.gamermvvmapp.presentation.screens.signUp
 
-import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.kotlin_crew.gamermvvmapp.R
+import com.kotlin_crew.gamermvvmapp.presentation.components.DefaultTopBar
+import com.kotlin_crew.gamermvvmapp.presentation.screens.signUp.components.SignUpContent
+import com.kotlin_crew.gamermvvmapp.presentation.ui.theme.GamerMVVMAppTheme
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun SignUpScreen(navController:NavHostController) {
+fun SignUpScreen(navController: NavHostController) {
     Scaffold(
-        topBar = {},
+        topBar = {
+            DefaultTopBar(
+                title = stringResource(id = R.string.topAppBar_SignUp),
+                upAvailable = true,
+                navController = navController
+            )
+        },
         content = {
-            Text(text = "SignUpScreen")
+            SignUpContent(it)
         },
         bottomBar = {}
     )
+}
+
+@Preview
+@Composable
+fun PreviewSignUpScreen() {
+    GamerMVVMAppTheme(darkTheme = true) {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+        ) {
+            SignUpScreen(rememberNavController())
+        }
+    }
 }
